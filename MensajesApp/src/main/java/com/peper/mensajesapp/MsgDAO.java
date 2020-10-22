1992d5ffea4e794e0a07f75552726222ed1c4847
Andrés Rios <peperuelo@gmail.com>
2020-10-22 12:18:40 -0500

    faltan solo unos retoques pero basicamente terminado

--- MensajesApp/src/main/java/com/peper/mensajesapp/MsgDAO.java
+++ MensajesApp/src/main/java/com/peper/mensajesapp/MsgDAO.java
@@ -7,6 +7,7 @@ package com.peper.mensajesapp;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 
@@ -32,13 +33,62 @@ public class MsgDAO {
             System.out.println(ex);
         }
     }
-    public static void readMsgDB(Message msg){
-        
-    }
+    public static void readMsgDB(){
+         ConnectionS dbConnect = new ConnectionS();
+         PreparedStatement ps = null;
+          ResultSet rs = null;
+          try(Connection cn = dbConnect.get_connection() ){
+            String query = "SELECT * from mensajes";
+            ps = cn.prepareStatement(query);
+            rs = ps.executeQuery(query);
+            
+            while(rs.next()){
+                System.out.println("ID: "+rs.getInt("id_mensaje"));
+                System.out.println("Mensaje: "+rs.getString("mensaje"));
+                System.out.println("Autor: "+rs.getString("autor_mensaje"));
+                System.out.println("Fecha: "+rs.getString("fecha_mensaje"));
+                System.out.println("");
+            }
+          }catch(SQLException e){
+                    System.out.println();   
+           
+               
+           }
+        }
+    
     public static void updateMsgDB(Message msg){
-        
+         ConnectionS dbConnect = new ConnectionS();
+         PreparedStatement ps = null;
+         ResultSet rs = null;
+         try(Connection cn = dbConnect.get_connection() ){
+            String query = "UPDATE mensajes SET mensaje =? WHERE id_mensaje  = ?";
+            ps = cn.prepareStatement(query);
+             ps.setString(1, msg.getMsg());
+            ps.setInt(2, msg.getId_msg());
+            ps.executeUpdate();
+            System.out.println(" Mensaje ha sido actualizado.");
+        }catch(SQLException ex){
+            System.out.println(ex);
+            System.out.println("No se pudo actualizar el mensaje"); 
+        }
     }
-    public void deleteMsgDB(Message msg){
+    
+    public static  void deleteMsgDB(int idMsg){
+              ConnectionS dbConnect = new ConnectionS();
+        PreparedStatement ps = null;
+        ResultSet rs = null;
         
+        try(Connection cn = dbConnect.get_connection() ){
+            String query = "DELETE from mensajes Where id_mensaje  = ?";
+            ps = cn.prepareStatement(query);
+            ps.setInt(1, idMsg);
+            ps.executeUpdate();
+            System.out.println(" Mensaje ha sido eliminado.");
+        }catch(SQLException e){
+               
+            System.out.println(e);
+            System.out.println("No se pudo borrar el mensaje");   
+           
+           }
     }
 }
--- MensajesApp/src/main/java/com/peper/mensajesapp/MsgService.java
+++ MensajesApp/src/main/java/com/peper/mensajesapp/MsgService.java
@@ -14,16 +14,27 @@ public class MsgService {
         Message register = new Message();
         register.setMsg(msg);
         register.setMsg_auth(name);
-        System.out.println("antes de llegar al DAO");
         MsgDAO.createMsgDB(register);
     }
     public static void readMsg(){
-        
+        MsgDAO.readMsgDB();
     }
     public static  void updateMsg(){
-        
+        Scanner sc = new Scanner(System.in);
+         System.out.println("escribe el mensaje");
+        String msg = sc.nextLine();
+        System.out.println("escribe el id del mensaje");
+        int id = sc.nextInt();
+        Message update = new Message();
+        update.setMsg(msg);
+        update.setId_msg(3);
+       
+        MsgDAO.updateMsgDB(update);
     }
     public static  void deleteMsg(){
-        
+        Scanner sc = new Scanner(System.in);
+        System.out.println("escribe el id del mensaje");
+        int id= sc.nextInt();
+        MsgDAO.deleteMsgDB(id);
     }
 }
